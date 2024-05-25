1101d2e47d570b87d98bb4783477bc6013bd0f9c
joe.meza <jmeza.sistemas@gmail.com>
2024-05-24 22:33:00 -0500

    subida de sidenav y toolbar

--- src/app/modules/shared/components/sidenav/sidenav.component.ts
+++ src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class SidenavComponent {
 
+  modileQuery : MediaQueryList;
+
+  menuNav = [
+    {name: "Home", route: "home", icon: "home"},
+    {name: "Categorías", route: "category", icon: "category"},
+    {name: "Productos", route: "product", icon: "production_quantity_limits"}
+  ]
+
+  constructor(media : MediaMatcher){
+    this.modileQuery = media.matchMedia('(max-width : 600px)');
+
+  }
+  
 }
