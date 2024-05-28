8a658d23ed89e30b7bb85d0543e7f94149b31977
jmeza.sistemas@gmail.com <jmeza.sistemas@gmail.com>
2024-05-28 18:07:03 -0500

    agregar submenus navbar

--- src/app/modules/shared/components/sidenav/sidenav.component.ts
+++ src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -1,10 +1,17 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
+  animations: [
+    trigger('expandPanel', [
+      state('true', style({ height: '*' })),
+      state('false', style({ height: '0px', visibility: 'hidden' })),
+      transition('true <=> false', animate('250ms ease-in-out')),
+    ])],
 })
 export class SidenavComponent {
 
@@ -12,7 +19,16 @@ export class SidenavComponent {
 
   menuNav = [
     {name: "Home", route: "home", icon: "home"},
-    {name: "Asiento RV", route: "asiento-rv", icon: "category"},
+    {name: "Asiento RV", icon: "category", children: [
+      {
+        name: "Asiento RV Contado", route: 'asiento-rv-contado', icon: "production_quantity_limits"
+      },
+      {
+        name: "Asiento RV Credito", route: 'asiento-rv-credito', icon: "production_quantity_limits"
+      }
+    ]
+
+    },
     {name: "Asientos RC", route: "asiento-rc", icon: "production_quantity_limits"}
   ]
 
