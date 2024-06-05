6c3a06c3d65ee285c791e3cf1c09e60a8fd65c5c
unknown <jmeza.sistemas@gmail.com>
2024-06-05 10:53:23 -0500

    asiento rv contado

--- src/app/modules/asientos-rv-contado/components/asiento-rv-contado/asiento-rv-contado.component.ts
+++ src/app/modules/asientos-rv-contado/components/asiento-rv-contado/asiento-rv-contado.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, Inject, OnInit, inject } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
+import { ToastrService } from 'ngx-toastr';
 import { dataAsientosRC } from 'src/app/core/models/asientos-rc/asiento-rc-request';
+import { SendAsientosService } from 'src/app/modules/shared/services/send-asientos.service';
 
 @Component({
   selector: 'app-asiento-rv-contado',
@@ -11,7 +13,8 @@ import { dataAsientosRC } from 'src/app/core/models/asientos-rc/asiento-rc-reque
 export class AsientoRvContadoComponent implements OnInit {
 
   private fb = inject(FormBuilder);
-
+  private asientoSendGenerico = inject(SendAsientosService);
+  private toast = inject(ToastrService);
 
   displayColumns : String[] = ['accountCode','subAccountCode','fundcode','functionCode','restriccionCode','entityValue','sendMemo','descripcion'];
   dataSource! : MatTableDataSource<dataAsientosRC>;
@@ -30,7 +33,39 @@ export class AsientoRvContadoComponent implements OnInit {
     if(localStorage.getItem('token')){
       this.formulario.get('token')?.setValue(localStorage.getItem('token'));
     }
-    throw new Error('Method not implemented.');
+  }
+
+  obtenerToken():void{
+    this.statusLoadingToken = true;
+    this.genTokenButtonText = 'Generando token';
+    this.textIconToken = 'lock_reset';
+    this.asientoSendGenerico.getToken().subscribe( response =>{
+
+      this.statusLoadingToken = false;    
+
+      if(response.metadata[0].code == "00"){
+
+        this.genTokenButtonText = 'token';
+        this.textIconToken = 'vpn_key';
+        this.toast.success(response.metadata[0].message,'Asientos Compras Dia') 
+        this.formulario.get("token")?.setValue(String(response.response['token']));  
+        localStorage.setItem("token", String(response.response['token']));          
+
+      }else{
+        this.toast.error('Error al cargar los datos', 'Mensaje de Error');
+      } 
+
+    },error =>{
+      this.genTokenButtonText = 'token';
+      this.textIconToken = 'vpn_key';
+      this.statusLoadingToken = false;
+      this.toast.error('Hubo problemas con el servidor',error.name)         
+    });
+    
+  }
+
+  listarAsientoContado():void{
+    
   }
 
   
